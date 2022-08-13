70443c1fc841aef245af3e97c15253befa24397b
Muhammet Vefa Keserci <81871784+Antoneia@users.noreply.github.com>
2022-08-13 12:35:01 +0300

    Initial commit

---
+++ src/test/java/com/example/Connection/SpringBootPostgreSqlConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgreSqlConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
